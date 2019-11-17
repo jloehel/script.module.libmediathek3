ecef56a0d8b41b19838ee3f6dd0b0afa9523920e
Jürgen Löhel <juergen@loehel.de>
2019-11-18 00:05:56 +0100

    python3 compatibility
     - Replaces urllib with python requests
     - autopep8 magic
    
    Signed-off-by: Jürgen Löhel <juergen@loehel.de>

--- lib/libmediathek3dialogs.py
+++ lib/libmediathek3dialogs.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-import urllib
-import urllib2
 import socket
 import sys
 import xbmc
@@ -10,20 +8,25 @@ import xbmcaddon
 import xbmcvfs
 import re
 from datetime import datetime
+from requests.compat import quote_plus
 import time
 
 import libmediathek3utils
 
+
 def dialogDate(format='%d%m%Y'):
-	dialog = xbmcgui.Dialog()
-	result = dialog.numeric(1, libmediathek3utils.getTranslation(31030)).replace("/", "").replace(" ", "0")
-	try:
-		result = datetime.strptime(result, '%d%m%Y')
-	except TypeError:  # handle bug in Python 2
-		result = datetime(*(time.strptime(result, '%d%m%Y')[0:6]))
-	return result.strftime(format)
-	
+    dialog = xbmcgui.Dialog()
+    result = dialog.numeric(1, libmediathek3utils.getTranslation(
+        31030)).replace("/", "").replace(" ", "0")
+    try:
+        result = datetime.strptime(result, '%d%m%Y')
+    except TypeError:  # handle bug in Python 2
+        result = datetime(*(time.strptime(result, '%d%m%Y')[0:6]))
+    return result.strftime(format)
+
+
 def getSearchString():
-	dialog = xbmcgui.Dialog()
-	d = dialog.input(libmediathek3utils.getTranslation(31039),type=xbmcgui.INPUT_ALPHANUM)
-	return urllib.quote_plus(d)
+    dialog = xbmcgui.Dialog()
+    d = dialog.input(libmediathek3utils.getTranslation(
+        31039), type=xbmcgui.INPUT_ALPHANUM)
+    return quote_plus(d)
--- lib/libmediathek3webvtt2srt.py
+++ lib/libmediathek3webvtt2srt.py
@@ -8,44 +8,48 @@ import xbmcaddon
 import HTMLParser
 import xbmcvfs
 
-subFile = xbmc.translatePath(xbmcaddon.Addon().getAddonInfo('profile')+'/webvtt.srt').decode('utf-8')
+subFile = xbmc.translatePath(xbmcaddon.Addon().getAddonInfo(
+    'profile')+'/webvtt.srt')
 
 bracketLookup = {
-	'<c.textWhite>':	'<font color="#ffffff">',
-	'<c.textYellow>':	'<font color="#ffff00">',
-	'<c.textCyan>':		'<font color="#00ffff">',
-	'<c.textRed>':		'<font color="#ff0000">',
-	'<c.textGreen>':	'<font color="#00ff00">',
-	'<c.textBlue>':		'<font color="#0000ff">',
-	'<c.textMagenta>':	'<font color="#ff00ff">',
-	'<c.textMagenta>':	'<font color="#ff00ff">',
-	'</c>':				'</font>',
+    '<c.textWhite>':	'<font color="#ffffff">',
+    '<c.textYellow>':	'<font color="#ffff00">',
+    '<c.textCyan>':		'<font color="#00ffff">',
+    '<c.textRed>':		'<font color="#ff0000">',
+    '<c.textGreen>':	'<font color="#00ff00">',
+    '<c.textBlue>':		'<font color="#0000ff">',
+    '<c.textMagenta>':	'<font color="#ff00ff">',
+    '<c.textMagenta>':	'<font color="#ff00ff">',
+    '</c>':				'</font>',
 }
+
+
 def webvtt2Srt(url):
-	if xbmcvfs.exists(subFile):
-		xbmcvfs.delete(subFile)
-	
-	webvtt = utils.getUrl(url)
-	s = webvtt.split('\n\n')
-	i = 1
-	srt = ''
-	while i < len(s):
-		j = 0
-		for line in s[i].split('\n'):
-			if j == 0:
-				srt += line + '\n'
-			elif j == 1:
-				t = line.split(' ')
-				srt += t[0][:-1].replace('.',',') + ' --> ' + t[2][:-1].replace('.',',') + '\n'
-			else:
-				for bracket in bracketLookup:
-					line = line.replace(bracket,bracketLookup[bracket])
-				srt += line + '\n'
-			j += 1
-		srt += '\n'
-		i += 1
-	
-	f = xbmcvfs.File(subFile, 'w')
-	f.write(srt)
-	f.close()
-	return subFile
+    if xbmcvfs.exists(subFile):
+        xbmcvfs.delete(subFile)
+
+    webvtt = utils.getUrl(url)
+    s = webvtt.split('\n\n')
+    i = 1
+    srt = ''
+    while i < len(s):
+        j = 0
+        for line in s[i].split('\n'):
+            if j == 0:
+                srt += line + '\n'
+            elif j == 1:
+                t = line.split(' ')
+                srt += t[0][:-1].replace('.', ',') + ' --> ' + \
+                    t[2][:-1].replace('.', ',') + '\n'
+            else:
+                for bracket in bracketLookup:
+                    line = line.replace(bracket, bracketLookup[bracket])
+                srt += line + '\n'
+            j += 1
+        srt += '\n'
+        i += 1
+
+    f = xbmcvfs.File(subFile, 'w')
+    f.write(srt)
+    f.close()
+    return subFile
